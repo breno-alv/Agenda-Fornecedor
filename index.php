a5b6f873a3d3a058427ad73ab6f9a816d015d3f6
breno-alv <breno.a01@gmail.com>
2023-09-05 17:49:43 -0300

    configuração layout do index

--- index.php
+++ index.php
@@ -22,9 +22,9 @@ if (isset($_POST['Novo'])) {
     $contato = $_POST['contato'];
     // var_dump($_POST);
 
-    if ($C_agenda->inserir($nome, $tipo_servico, $natureza, $vencimento, $valor, $forma_pgt, $periodicidade, $contato )) {
-        // header('Location: admin-Cargo.php?ins=1');
-        echo "Adicionado com sucesso";
+    if ($C_agenda->inserir($nome, $tipo_servico, $natureza, $vencimento, $valor, $forma_pgt, $periodicidade, $contato)) {
+        header('Location: index.php?ins=1');
+        // echo "Adicionado com sucesso";
     } else {
         // header('Location: admin-Cargo.php?ins=0');
         echo "Não foi adicionado";
@@ -45,7 +45,7 @@ if (isset($_POST['Editar'])) {
     $id = $_POST['id'];
     var_dump($_POST);
 
-    if ($C_agenda->editar($nome, $tipo_servico, $natureza, $vencimento, $valor, $forma_pgt, $periodicidade, $contato, $id )){
+    if ($C_agenda->editar($nome, $tipo_servico, $natureza, $vencimento, $valor, $forma_pgt, $periodicidade, $contato, $id)) {
         echo "Fornecedor Editado";
     } else {
         echo "Erro ao editar o Fornecedor";
@@ -55,104 +55,114 @@ if (isset($_POST['Editar'])) {
 ?>
 
 <div class="container">
-    <?php if (isset($printMsg) && $printMsg != '') : ?>
+    <!-- <?php if (isset($printMsg) && $printMsg != '') : ?>
         <p id="msg"><?= $printMsg ?></p>
-    <?php endif; ?>
-
-    <h1 id="main-title" class="text-center m-3">Fornecedores</h1>
-    <?php if (count($agenda) > 0) : ?>
-        <!-- <p>Tem Contatos</p> -->
-        <div class="card card-solid">
-            <div class="card-body">
-                <table class="table table-hover" id="tabela-agenda">
-                    <thead>
-                        <tr>
-                            <th>
-                                <center>#<center>
-                            </th>
-                            <th>
-                                <center>Nome<center>
-                            </th>
-                            <th>
-                                <center>Serviço<center>
-                            </th>
-                            <th>
-                                <center>Natureza<center>
-                            </th>
-                            <th>
-                                <center>Vencimento</center>
-                            </th>
-                            <th>
-                                <center>Valor</center>
-                            </th>
-                            <th>
-                                <center>Forma Pagamento</center>
-                            </th>
-                            <th>
-                                <center>Periodicidade</center>
-                            </th>
-                            <th>
-                                <center>Contato</center>
-                            </th>
-                            <th>
-                                <center>Açoes</center>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($agenda as $a) { ?>
-                            <tr>
-                                <!-- <th scope="row">1</th> -->
-                                <td>
-                                    <center><?= $a['id']; ?></center>
-                                </td>
-                                <td>
-                                    <center><?= $a['nome']; ?></center>
-                                </td>
-                                <td>
-                                    <center><?= $a['tipo_servico']; ?></center>
-                                </td>
-                                <td>
-                                    <center><?= $a['natureza']; ?></center>
-                                </td>
-                                <td>
-                                    <center><?= $a['vencimento']; ?></center>
-                                </td>
-                                <!-- <td>
-                                    <center><?= $a['tipo_servico']; ?></center>
-                                </td> -->
-                                <td>
-                                    <center><?= $a['valor']; ?></center>
-                                </td>
-                                <td>
-                                    <center><?= $a['forma_pgt']; ?></center>
-                                </td>
-                                <!-- <td>
-                                    <center><?= $a['tipo_servico']; ?></center>
-                                </td> -->
-                                <td>
-                                    <center><?= $a['periodicidade']; ?></center>
-                                </td>
-                                <td>
-                                    <center><?= $a['contato']; ?></center>
-                                </td>
-                                <td class="actions d-flex justify-content-around">
-                                    <div>
-                                        <center><button title="Editar" type="button" class="btn btn-warning" data-toggle="modal" data-target="#Editar<?= $a['id']; ?>"><i class="fa fa-edit"></i></button></center>
-                                    </div>
-                                    <div>
-                                        <center><button title="Deletar" type="button" class="btn btn-danger" data-toggle="modal" data-target="#Excluir<?= $a['id']; ?>"><i class="fas fa-trash"></i></button></center>
-                                    </div>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
+    <?php endif; ?> -->
+    <?php if (isset($_GET['ins']) and $_GET['ins'] == 1) { ?>
+        <div class="alert alert-success alert-dismissible fade show mx-auto w-50 p-3 text-center mt-3" role="alert">
+            ADICIONADO COM SUCESSO!
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-    <?php else : ?>
-        <p id="empty-list-text">Ainda Não há Contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para Adicionar</a>.</p>
-    <?php endif; ?>
+    <?php } ?>
+</div>
+
+
+<h1 id="main-title" class="text-center m-3">Fornecedores</h1>
+<?php if (count($agenda) > 0) : ?>
+    <!-- <p>Tem Contatos</p> -->
+    <div class="card card-solid m-5 ">
+        <div class="card-body bg-light ">
+            <table class="table table-hover table-light" id="tabela-agenda">
+                <thead>
+                    <tr>
+                        <th>
+                            <center>#<center>
+                        </th>
+                        <th>
+                            <center>Nome<center>
+                        </th>
+                        <th>
+                            <center>Serviço<center>
+                        </th>
+                        <th>
+                            <center>Natureza<center>
+                        </th>
+                        <th>
+                            <center>Vencimento</center>
+                        </th>
+                        <th>
+                            <center>Valor</center>
+                        </th>
+                        <th>
+                            <center>Forma Pagamento</center>
+                        </th>
+                        <th>
+                            <center>Periodicidade</center>
+                        </th>
+                        <th>
+                            <center>Contato</center>
+                        </th>
+                        <th>
+                            <center>Açoes</center>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($agenda as $a) { ?>
+                        <tr>
+                            <!-- <th scope="row">1</th> -->
+                            <td>
+                                <center><?= $a['id']; ?></center>
+                            </td>
+                            <td>
+                                <center><?= $a['nome']; ?></center>
+                            </td>
+                            <td>
+                                <center><?= $a['tipo_servico']; ?></center>
+                            </td>
+                            <td>
+                                <center><?= $a['natureza']; ?></center>
+                            </td>
+                            <td>
+                                <center><?= $a['vencimento']; ?></center>
+                            </td>
+                            <!-- <td>
+                                    <center><?= $a['tipo_servico']; ?></center>
+                                </td> -->
+                            <td>
+                                <center><?= $a['valor']; ?></center>
+                            </td>
+                            <td>
+                                <center><?= $a['forma_pgt']; ?></center>
+                            </td>
+                            <!-- <td>
+                                    <center><?= $a['tipo_servico']; ?></center>
+                                </td> -->
+                            <td>
+                                <center><?= $a['periodicidade']; ?></center>
+                            </td>
+                            <td>
+                                <center><?= $a['contato']; ?></center>
+                            </td>
+                            <td class="actions d-flex justify-content-around">
+                                <div>
+                                    <center><button title="Editar" type="button" class="btn btn-warning mr-1" data-toggle="modal" data-target="#Editar<?= $a['id']; ?>"><i class="fa fa-edit"></i></button></center>
+                                </div>
+                                <div>
+                                    <center><button title="Deletar" type="button" class="btn btn-danger" data-toggle="modal" data-target="#Excluir<?= $a['id']; ?>"><i class="fas fa-trash"></i></button></center>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+<?php else : ?>
+    <p id="empty-list-text">Ainda Não há Contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para Adicionar</a>.</p>
+<?php endif; ?>
 </div>
 
 <!--MODAL NOVO-->
@@ -177,37 +187,37 @@ if (isset($_POST['Editar'])) {
                         <input type="text" class="form-control" name="tipo_servico" id="tipo_servico" required="">
                         <br>
                     </div>
-                   
+
                     <div class="row">
                         <label for="natureza">Natureza:</label>
                         <input type="text" class="form-control" name="natureza" id="natureza" required="">
                         <br>
                     </div>
-                    
+
                     <div class="row">
                         <label for="vencimento">Vencimento:</label>
                         <input type="text" class="form-control" name="vencimento" id="vencimento" required="">
                         <br>
                     </div>
-                    
+
                     <div class="row">
                         <label for="valor">Valor:</label>
                         <input type="text" class="form-control" name="valor" id="valor" required="">
                         <br>
                     </div>
-                    
+
                     <div class="row">
                         <label for="forma_pgt">Forma Pagamento:</label>
                         <input type="text" class="form-control" name="forma_pgt" id="forma_pgt" required="">
                         <br>
                     </div>
-                 
+
                     <div class="row">
                         <label for="periodicidade">Periodicidade:</label>
                         <input type="text" class="form-control" name="periodicidade" id="preiodicidade" required="">
                         <br>
                     </div>
-                    
+
                     <div class="row">
                         <label for="contato">Contato:</label>
                         <input type="text" class="form-control" name="contato" id="contato" required="">
@@ -230,7 +240,7 @@ if (isset($_POST['Editar'])) {
             <div class="modal-content">
                 <form action="index.php" method="POST">
                     <input type="hidden" name="id" value="<?= $a['id']; ?>">
-                    <div class="modal-header" style="text-align: center;">
+                    <div class="modal-header" >
                         <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
                         <h2 class="modal-title" id="myModelLabel">
                             <center>EDITAR FORNECEDOR</center>
--- templates/header.php
+++ templates/header.php
@@ -27,6 +27,7 @@ if(isset($_SESSION['msg'])){
     <!-- Font Awesome Fontes, icones Editar, Visualizar e Deletar-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.0/css/all.min.css" integrity="sha512-UJqci0ZyYcQ0AOJkcIkUCxLS2L6eNcOr7ZiypuInvEhO9uqIDi349MEFrqBzoy1QlfcjfURHl+WTMjEdWcv67A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
     <title>Lista de Fornecedores</title>
 </head>
